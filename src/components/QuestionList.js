5d553be11c5e96b8e73660e7c06bb012247c2ef6
labankibet <laban.kibet@moringaschool.com>
2023-08-27 00:27:07 +0300

    display questions using get request

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from the API when the component mounts
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>{question.text}</li>
+        ))}
+      </ul>
     </section>
   );
 }
