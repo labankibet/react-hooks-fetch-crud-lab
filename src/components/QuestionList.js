5be13616e18c825f7ec1ac3ce716aec410fcf664
labankibet <laban.kibet@moringaschool.com>
2023-08-31 10:49:02 +0300

    feching from an api

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,23 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    // Fetch questions from the API when the component mounts
     fetch("http://localhost:4000/questions")
-      .then((response) => response.json())
-      .then((data) => setQuestions(data))
+      .then((r) => r.json())
+      .then((questions) => {
+        setQuestions(questions);
+      });
   }, []);
+
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        setQuestions((prevQuestions) =>
+          prevQuestions.filter((q) => q.id !== id)
+        );
+      });
+  }  
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        setQuestions((prevQuestions) =>
+          prevQuestions.map((q) => {
+            if (q.id === updatedQuestion.id) return updatedQuestion;
+            return q;
+          })
+        );
+      });
+  }
+    const questionItems = questions.map((q) => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+      onDeleteClick={handleDeleteClick}
+      onAnswerChange={handleAnswerChange}
+    />
+  ));
 
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>
-        {questions.map((question) => (
-          <li key={question.id}>{question.text}</li>
-        ))}
-      </ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
